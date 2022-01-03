13f3ecf548e47ed74d0721cf1f89b03166807de9
Dima <yadima2001@gmail.com>
2022-01-03 22:11:03 +0200

    helpers

---
+++ Module3HW7/Helpers/Starter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Module3HW7.Services;
+using Module3HW7.Services.Abstractions;
+
+namespace Module3HW7.Helpers
+{
+    public class Starter
+    {
+        public void Run()
+        {
+            var starter = new ServiceCollection()
+                .AddSingleton<ILoggerService, LoggerService>()
+                .AddSingleton<IBackupService, BackupService>()
+                .AddSingleton<IFileService, FileService>()
+                .AddSingleton<IJsonService, JsonService>()
+                .AddSingleton<IConfigService, ConfigService>()
+                .AddScoped<Application>()
+                .BuildServiceProvider();
+            var app = starter.GetService<Application>();
+            app.Run();
+            starter.Dispose();
+        }
+    }
+}
