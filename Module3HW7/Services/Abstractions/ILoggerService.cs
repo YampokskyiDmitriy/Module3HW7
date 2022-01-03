e60d36ceeda6c57c61363e3bc7f09efae56610d3
Dima <yadima2001@gmail.com>
2022-01-03 22:11:55 +0200

    services

---
+++ Module3HW7/Services/Abstractions/ILoggerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Module3HW7.Models;
+
+namespace Module3HW7.Services.Abstractions
+{
+    public interface ILoggerService
+    {
+        public event Action<SemaphoreSlim, string> OnBackup;
+        public Task LogAsync(TypeLog typeLog, string message);
+    }
+}
---
+++ Module3HW7/Services/JsonService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Module3HW7.Services.Abstractions;
+using Newtonsoft.Json;
+
+namespace Module3HW7.Services
+{
+    public class JsonService : IJsonService
+    {
+        public async Task<object> DeserializationAsync(string jsonObj, Type type)
+        {
+            return await Task.Run(() =>
+                {
+                    return JsonConvert.DeserializeObject(jsonObj, type);
+                });
+        }
+    }
+}
